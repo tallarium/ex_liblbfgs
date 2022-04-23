65f4ab9203ba1fc14d16d199df3e7fe3ef160635
Matthew Whitworth <matt@tallarium.com>
2022-04-23 08:33:10 +0100

    initial commit

---
+++ lib/ex_liblbfgs.ex
@@ -0,0 +1,18 @@
+defmodule ExLiblbfgs do
+  @moduledoc """
+  Documentation for `ExLiblbfgs`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExLiblbfgs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_liblbfgs_test.exs
@@ -0,0 +1,8 @@
+defmodule ExLiblbfgsTest do
+  use ExUnit.Case
+  doctest ExLiblbfgs
+
+  test "greets the world" do
+    assert ExLiblbfgs.hello() == :world
+  end
+end
